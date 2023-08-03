270ac786c7066e9516184660123d78decd831c8e
Springer <zxllks@yahoo.com>
2023-08-03 11:25:33 +0800

    L3 useForm validationSchema

--- src/AppValidate.vue
+++ src/AppValidate.vue
@@ -2,20 +2,31 @@
   <form @submit.prevent="onSubmit">
     <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
-    <BaseInput label="Password" type="password" />
+    <BaseInput label="Password" type="password" v-model="password" :error="passwordError" />
 
     <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
   </form>
 </template>
 
 <script setup lang="ts">
-import { useField } from 'vee-validate'
+import { useField, useForm } from 'vee-validate'
 
-const { value: email, errorMessage: emailError } = useField<string>('email', (value) => {
-  if (!value) return 'Email is Required'
-  if (value.length <= 3) return 'Email is too short'
-  return true
-})
+const schema = {
+  email: (value: string) => {
+    if (!value) return 'Email is Required'
+    if (value.length <= 3) return 'Email is too short'
+    return true
+  },
+  password: (value: string) => {
+    if (!value) return 'This Field is Required'
+    return true
+  }
+}
+
+useForm({ validationSchema: schema })
+
+const { value: email, errorMessage: emailError } = useField<string>('email')
+const { value: password, errorMessage: passwordError } = useField<string>('password')
 
 function onSubmit() {
   alert('Submitted')
