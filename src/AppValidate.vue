079e050f602de28fe4e90497ca7cd0d870123645
Springer <zxllks@yahoo.com>
2023-08-03 10:54:55 +0800

    L1 setup for form validate

---
+++ src/AppValidate.vue
@@ -0,0 +1,15 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <BaseInput label="Email" type="email" />
+
+    <BaseInput label="Password" type="password" />
+
+    <BaseButton type="submit" class="-fill-gradient"> Submit </BaseButton>
+  </form>
+</template>
+
+<script setup lang="ts">
+function onSubmit() {
+  alert('Submitted')
+}
+</script>
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    BaseButton: typeof import('./../components/BaseButton.vue')['default']
     BaseCheckbox: typeof import('./../components/BaseCheckbox.vue')['default']
     BaseInput: typeof import('./../components/BaseInput.vue')['default']
     BaseRadioGroup: typeof import('./../components/BaseRadioGroup.vue')['default']
