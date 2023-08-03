a7ce93a384843adb43735df73c4bb0219149e367
Springer <zxllks@yahoo.com>
2023-08-03 11:07:10 +0800

    L2 vee-validate CAPI useField

--- src/AppValidate.vue
+++ src/AppValidate.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <BaseInput label="Email" type="email" />
+    <BaseInput label="Email" type="email" v-model="email" :error="emailError" />
 
     <BaseInput label="Password" type="password" />
 
@@ -9,6 +9,14 @@
 </template>
 
 <script setup lang="ts">
+import { useField } from 'vee-validate'
+
+const { value: email, errorMessage: emailError } = useField<string>('email', (value) => {
+  if (!value) return 'Email is Required'
+  if (value.length <= 3) return 'Email is too short'
+  return true
+})
+
 function onSubmit() {
   alert('Submitted')
 }
